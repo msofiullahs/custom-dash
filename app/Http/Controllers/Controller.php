7275d72cb96e0d47363ae67f19fd7a94f79053b6
Sofiullah <sofiullah.work@gmail.com>
2024-04-05 03:12:40 +0700

    base log

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\ActivityLog;
 
 abstract class Controller
 {
-    //
+
+    protected function storeActivity($userId = null, $act)
+    {
+        $log = new ActivityLog();
+        $log->user_id = $userId;
+        $log->activity = json_encode($act);
+        $log->save();
+    }
+
 }
